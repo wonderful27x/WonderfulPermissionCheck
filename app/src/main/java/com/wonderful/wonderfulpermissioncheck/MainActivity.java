b74aaee377699f54070a6b0ec73fb4dc48505ed1
wonderful <wonderful27x@126.com>
2020-09-30 13:04:08 +0800

    优化gradle

--- app/src/main/java/com/wonderful/wonderfulpermissioncheck/MainActivity.java
+++ app/src/main/java/com/wonderful/wonderfulpermissioncheck/MainActivity.java
@@ -13,8 +13,6 @@ import androidx.appcompat.app.AppCompatActivity;
  */
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
